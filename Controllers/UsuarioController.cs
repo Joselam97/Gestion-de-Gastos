d71be55e0aee2b8acc7621fcd46ab8a957ffc304
Jose Alvarez <jlam97@hotmail.es>
2025-07-23 20:51:37 -0600

    Creacion clase Grupo, carpeta de imagenes para registro

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -5,39 +5,56 @@ using System.Text;
 using System.Threading.Tasks;
 using GestionDeGastos.Data;
 using GestionDeGastos.Models;
+using GestionDeGastos.Data;
+using GestionDeGastos.Models;
 
 namespace GestionDeGastos.Controllers
 {
- 
-        public class UsuarioController
-        {
+
+    public class UsuarioController
+    {
         //metodo que registra usuarios
-            public string RegistrarUsuario(string nombre, string correo, string contrasena)
-            {
+        public string RegistrarUsuario(string nombre, string correo, string contrasena, string rutaImagen)
+        {
             //carga la lista de usuarios guardados en JSON
-                List<Usuario> usuarios = UsuarioData.CargarUsuarios();
+            List<Usuario> usuarios = UsuarioData.CargarUsuarios();
 
             //verifica si existe un usuario con ese correo
-                bool existeCorreo = usuarios.Exists(u => u.Correo.ToLower() == correo.ToLower());
-                if (existeCorreo)
-                {
-                    return "Ya existe un usuario con ese correo!";
-                }
+            bool existeCorreo = usuarios.Exists(u => u.Correo.ToLower() == correo.ToLower());
+            if (existeCorreo)
+            {
+                return "Ya existe un usuario con ese correo!";
+            }
 
-                //crea usuario con los datos ingresados
-                Usuario nuevoUsuario = new Usuario
-                {
-                    Nombre = nombre,
-                    Correo = correo,
-                    Contrasena = contrasena
-                };
+            //crea usuario con los datos ingresados
+            Usuario nuevoUsuario = new Usuario
+            {
+                Nombre = nombre,
+                Correo = correo,
+                Contrasena = contrasena,
+                RutaImagen = rutaImagen
+            };
 
             //agrega usuario a la lista existente
-                usuarios.Add(nuevoUsuario);
-                UsuarioData.GuardarUsuarios(usuarios);
+            usuarios.Add(nuevoUsuario);
+            UsuarioData.GuardarUsuarios(usuarios);
 
             //mensaje devuelto
-                return "Usuario registrado exitosamente";
-            }
+            return "Usuario registrado exitosamente";
+        }
+
+        public string ValidarInicioSesion(string correo, string contrasena)
+        {
+            List<Usuario> usuarios = UsuarioData.CargarUsuarios();
+
+            var usuario = usuarios.FirstOrDefault(u =>
+                u.Correo.ToLower() == correo.ToLower() &&
+                u.Contrasena == contrasena);
+
+            if (usuario == null)
+                return "Correo o contraseña incorrectos.";
+
+            return "OK";
         }
     }
+}
